1640f877749031cec1c2785b980b3874d3d992d0
Peter Bjorklund <piot@hotmail.com>
2025-06-07 22:58:24 +0200

    fix: block comment

--- crates/lexer/tests/test.rs
+++ crates/lexer/tests/test.rs
@@ -16,7 +16,7 @@ fn identifier() {
 
 fn get_identifier(lexer: &mut Lexer) -> String {
     let tok = lexer.next_token();
-    if let TokenKind::Identifier = tok.kind {
+    if tok.kind == TokenKind::Identifier {
         lexer.get_string(&tok)
     } else {
         panic!("not a string")
@@ -28,7 +28,7 @@ fn get_string(lexer: &mut Lexer) -> String {
     if tok.kind == TokenKind::StringLiteral {
         lexer.get_string(&tok)
     } else {
-        panic!("not a string")
+        panic!("not a string {tok:?}")
     }
 }
 
@@ -41,21 +41,48 @@ fn get_string_start(lexer: &mut Lexer) -> String {
     }
 }
 
+fn get_doc_comment(lexer: &mut Lexer) -> String {
+    let tok = lexer.next_token();
+    if tok.kind == TokenKind::DocComment {
+        lexer.get_string(&tok)
+    } else {
+        panic!("get_doc_comment {tok:?}")
+    }
+}
+
+fn get_block_comment(lexer: &mut Lexer) -> String {
+    let tok = lexer.next_token();
+    if tok.kind == TokenKind::BlockComment {
+        lexer.get_string(&tok)
+    } else {
+        panic!("get_block_comment {tok:?}")
+    }
+}
+
+fn get_line_comment(lexer: &mut Lexer) -> String {
+    let tok = lexer.next_token();
+    if tok.kind == TokenKind::LineComment {
+        lexer.get_string(&tok)
+    } else {
+        panic!("get_block_comment {tok:?}")
+    }
+}
+
 fn get_string_middle(lexer: &mut Lexer) -> String {
     let tok = lexer.next_token();
     if tok.kind == TokenKind::StringMiddle {
         lexer.get_string(&tok)
     } else {
-        panic!("not a string")
+        panic!("not a get_string_middle {tok:?}")
     }
 }
 
 fn get_string_end(lexer: &mut Lexer) -> String {
     let tok = lexer.next_token();
-    if let TokenKind::StringEnd = tok.kind {
+    if tok.kind == TokenKind::StringEnd {
         lexer.get_string(&tok)
     } else {
-        panic!("not a string")
+        panic!("not a string end {tok:?}")
     }
 }
 
@@ -119,4 +146,39 @@ fn string_interpolation() {
     assert_eq!(TokenKind::RBrace, lexer.next_token().kind);
     assert_eq!(get_string_end(&mut lexer), " world ",);
     assert_eq!(TokenKind::EOF, lexer.next_token().kind);
+}
+
+#[test]
+fn test_line_comment() {
+    let mut lexer = Lexer::new("  // This is a comment\nx = 1");
+
+    assert_eq!(get_line_comment(&mut lexer), "// This is a comment");
+    assert_eq!(get_identifier(&mut lexer), "x");
+}
+
+#[test]
+fn test_doc_comment() {
+    let mut lexer = Lexer::new("    /// Documentation\nfn test() {}");
+
+    assert_eq!(get_doc_comment(&mut lexer), "/// Documentation");
+    assert_eq!(get_identifier(&mut lexer), "fn");
+}
+
+#[test]
+fn test_block_comment() {
+    let mut lexer = Lexer::new(" /* Block\ncomment\n*/\nx = 1");
+
+    assert_eq!(get_block_comment(&mut lexer), "/* Block\ncomment\n*/");
+    assert_eq!(get_identifier(&mut lexer), "x");
+}
+
+#[test]
+fn test_unterminated_block_comment() {
+    let mut lexer = Lexer::new("/* Unterminated");
+
+    if let TokenKind::Unknown('*') = lexer.next_token().kind {
+        // Test passed
+    } else {
+        panic!("Expected unknown token for unterminated block comment");
+    }
 }
